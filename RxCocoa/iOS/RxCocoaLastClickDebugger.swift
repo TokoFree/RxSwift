bceb56a0fc8859d5f6a0e76e69e15ef7c71c6f0f
Bondan Prasetyo <bondan.prasetyo@bytedance.com>
2025-03-18 01:29:09 +0700

    blacklist

--- RxCocoa/iOS/RxCocoaLastClickDebugger.swift
+++ RxCocoa/iOS/RxCocoaLastClickDebugger.swift
@@ -3,7 +3,7 @@ import Foundation
 public struct RxCocoaLastClickDebugger {
     private static var className: String = ""
 
-    public static var whitelistClassName: (() -> [String])?
+    public static var blacklistClassName: (() -> [String])?
 
     public static func getClassName() -> String {
         defer {
@@ -24,13 +24,13 @@ public struct RxCocoaLastClickDebugger {
     }
 
     public static func findInherenceNode(in view: UIView) -> String? {
-        guard let superview = view.superview, let whitelist = whitelistClassName?() else {
+        guard let superview = view.superview, let blacklist = blacklistClassName?() else {
             return nil
         }
 
         let stringName = superview.description
 
-        if !whitelist.contains(stringName) {
+        if !blacklist.contains(stringName) {
             return stringName
         }
 
