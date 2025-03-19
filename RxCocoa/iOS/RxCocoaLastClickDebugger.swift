ddbd8c4dc6341f4a0ce6877360bfd56402172ec1
Bondan Prasetyo <bondan.prasetyo@bytedance.com>
2025-03-19 15:05:21 +0700

    update api

--- RxCocoa/iOS/RxCocoaLastClickDebugger.swift
+++ RxCocoa/iOS/RxCocoaLastClickDebugger.swift
@@ -1,25 +1,38 @@
 import Foundation
+import UIKit
 
 public struct RxCocoaLastClickDebugger {
     private static var className: String = ""
 
     public static var skipClassName: (() -> [String])?
 
+    /*
+        Get the last clicked class name
+    */
     public static func getClassName() -> String {
         return Self.className
     }
 
+    /*
+        Set the last clicked class name
+    */
     public static func setClassName(_ className: String) {
         // Reset State
         Self.className = ""
         Self.className = className
     }
-    
+
+    /*
+        Appending setted class name
+    */
     public static func appendClassName(_ className: String) {
         let appendText = "\(Self.className)-\(className)"
         Self.className = appendText
     }
 
+    /*
+        This Function is used to recursively until get meaningfull class name
+    */
     public static func findInherenceNode(in view: UIView) -> String? {
         guard let superview = view.superview, let skiplist = skipClassName?() else {
             return nil
